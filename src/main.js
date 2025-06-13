b2f36148de9c469957be2ee670b7e00399c467fd
陈成 <cheng@tanmer.com>
2025-06-13 15:24:28 +0800

    发布2.0.1；界面优化

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import * as Turbo from "@hotwired/turbo"
-import Alpine from 'alpinejs'
-import collapse from '@alpinejs/collapse'
-
-window.Alpine = Alpine
-Alpine.plugin(collapse)
-Alpine.start()
-
-import "./controllers"
